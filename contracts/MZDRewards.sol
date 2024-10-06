66caa5ad745161f347ed83eeb81bb9c2cdae5d4e
mazode <mrmaazkhan191@gmail.com>
2024-10-06 20:58:19 +0500

    setup the rewards contract

--- contracts/MZDRewards.sol
+++ contracts/MZDRewards.sol
@@ -11,6 +11,18 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 pragma solidity ^0.8.20;
 
 contract MZDRewards is ERC20, ERC20Burnable, Ownable, AccessControl {
-    
+    using SafeMath for uint256;
+    using SafeERC20 for ERC20;
+
+    mapping(address => uint256) private _balance;
+
+    uint256 private _totalSupply;
+
+    bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE"); 
+
+    constructor() ERC20("MZD Rewards", "MZDR") {
+        _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
+        _setupRole(MANAGER_ROLE, _msgSender());
+    }
 }
 
