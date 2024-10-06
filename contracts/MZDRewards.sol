6f3aec6183e24833414d5e573bbe97d5d9007a2d
mazode <mrmaazkhan191@gmail.com>
2024-10-06 20:50:58 +0500

    added rewards contract

---
+++ contracts/MZDRewards.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+pragma solidity ^0.8.20;
+
+contract MZDRewards is ERC20, ERC20Burnable, Ownable, AccessControl {
+    
+}
+
