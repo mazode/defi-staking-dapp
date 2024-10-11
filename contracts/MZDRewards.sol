c13de7e10f0157772884c008948c04c724ee51d7
mazode <mrmaazkhan191@gmail.com>
2024-10-11 21:55:49 +0500

    refactored the Reward contract

--- contracts/MZDRewards.sol
+++ contracts/MZDRewards.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT LICENSE
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
@@ -10,36 +10,32 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 
 pragma solidity ^0.8.20;
 
-contract MZDRewards is ERC20, ERC20Burnable, Ownable, AccessControl {
+contract MZDRewards is ERC20, ERC20Burnable, AccessControl, Ownable {
     using SafeERC20 for ERC20;
 
-    mapping(address => uint256) private _balance;
-
-    uint256 private _totalSupply;
-
+    // Define manager role
     bytes32 public constant MANAGER_ROLE = keccak256("MANAGER_ROLE"); 
 
+    // Constructor setting up roles and ownership
     constructor(address initialOwner) ERC20("MZD Rewards", "MZDR") Ownable(initialOwner) {
-        grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
-        grantRole(MANAGER_ROLE, initialOwner);
+        _grantRole(DEFAULT_ADMIN_ROLE, initialOwner);
+        _grantRole(MANAGER_ROLE, initialOwner);
     }
 
+    // Mint tokens, restricted to managers
     function mint(address to, uint256 amount) external {
         require(hasRole(MANAGER_ROLE, msg.sender), "Not allowed"); 
-        _totalSupply += amount;
-        _balance[to] += amount;
         _mint(to, amount);
     }
 
+    // Safe token transfer, restricted to managers
     function safeMzdTransfer(address _to, uint256 _amount) external {
         require(hasRole(MANAGER_ROLE, msg.sender), "Not allowed");
         uint256 mzdBalance = balanceOf(address(this));
-        
-        if(_amount > mzdBalance) {
+        if (_amount > mzdBalance) {
             transfer(_to, mzdBalance);
         } else {
             transfer(_to, _amount);
         }
     }
 }
-
